2ec34f65abfd0e763980f7bb31ff07a5733abdbb
Edward Heath <e42291+github@gmail.com>
2019-01-04 19:22:46 -0800

    Add board and game logic

--- tic_tac_toe.rb
+++ tic_tac_toe.rb
@@ -10,14 +10,22 @@ end
 
 class Board
     def init
-        @board = [[0,0,0],
-                  [0,0,0],
-                  [0,0,0]]
+        @board = [0,0,0,
+                  0,0,0,
+                  0,0,0]
     end
 
     def display
-        puts "#{@board[0][0]}"
+        puts "#{@board[0]}|#{@board[1]}|#{@board[2]}
+              -----
+              #{@board[3]}|#{@board[4]}|#{@board[5]}
+              -----
+              #{@board[6]}|#{@board[7]}|#{@board[8]}"
     end
 end
 
 game = Game.new
+game.display
+
+board = Board.new
+board.display
